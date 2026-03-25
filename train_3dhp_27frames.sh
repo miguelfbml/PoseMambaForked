dee7e75db263d3ef8a335ff712f79890fa1299b6
Miguel Lima <up202108659@up.pt>
2026-03-25 18:15:12 +0000

    shell

--- train_3dhp_27frames.sh
+++ train_3dhp_27frames.sh
@@ -7,4 +7,4 @@
 #SBATCH --error=slurm_%x.%j.err    # File containing STDERR output. If ommited, use STDOUT.
 echo "Starting training job for PoseMamba on MPI-INF-3DHP"
 
-python train_3dhp.py --config configs/pose3d/PoseMamba_train_3dhp_S.yaml --checkpoint checkpoint/pose3d/PoseMamba_train_3dhp_S 
+python train_3dhp.py --config configs/pose3d/PoseMamba_train_3dhp_S_27.yaml --checkpoint checkpoint/pose3d/PoseMamba_train_3dhp_S_27 
--- train_3dhp_9frames.sh
+++ train_3dhp_9frames.sh
@@ -8,4 +8,4 @@
 
 # Training script for PoseMamba-S on MPI-INF-3DHP with 9-frame temporal window
 
-python train_3dhp.py --config configs/pose3d/PoseMamba_train_3dhp_S_9frames.yaml --checkpoint checkpoint/pose3d/PoseMamba_train_3dhp_S_9frames
+python train_3dhp.py --config configs/pose3d/PoseMamba_train_3dhp_S_9.yaml --checkpoint checkpoint/pose3d/PoseMamba_train_3dhp_S_9

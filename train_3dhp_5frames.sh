2ce45d39e2ed47cc563d59bc706a258ffe7db65e
Miguel Lima <up202108659@up.pt>
2026-03-29 23:28:15 +0100

    n files

--- train_3dhp_5frames.sh
+++ train_3dhp_5frames.sh
@@ -2,9 +2,10 @@
 #
 #SBATCH --partition=gpu_min24gb     # Reserved partition
 #SBATCH --qos=gpu_min24gb           # QoS level. Must match the partition name. External users must add the suffix "_ext".
-#SBATCH --job-name=train3dhpS27Frames    # Job name
+#SBATCH --job-name=train3dhpS5Frames    # Job name
 #SBATCH --output=slurm_%x.%j.out   # File containing STDOUT output
 #SBATCH --error=slurm_%x.%j.err    # File containing STDERR output. If ommited, use STDOUT.
-echo "Starting training job for PoseMamba on MPI-INF-3DHP"
 
-python train_3dhp.py --config configs/pose3d/PoseMamba_train_3dhp_S_27.yaml --checkpoint checkpoint/pose3d/PoseMamba_train_3dhp_S_27 
+# Training script for PoseMamba-S on MPI-INF-3DHP with 5-frame temporal window
+
+python train_3dhp.py --config configs/pose3d/PoseMamba_train_3dhp_S_5.yaml --checkpoint checkpoint/pose3d/PoseMamba_train_3dhp_S_5

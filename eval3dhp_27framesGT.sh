0872818636b87c24b3664ea78a6cf2055313b257
Miguel Lima <up202108659@up.pt>
2026-05-20 19:28:16 +0100

    a

--- eval3dhp_27framesGT.sh
+++ eval3dhp_27framesGT.sh
@@ -2,11 +2,11 @@
 #
 #SBATCH --partition=gpu_min8gb     # Reserved partition
 #SBATCH --qos=gpu_min8gb           # QoS level. Must match the partition name. External users must add the suffix "_ext".
-#SBATCH --job-name=testPoseMamba3DHP_S_27    # Job name
+#SBATCH --job-name=testPoseMamba3DHP_S_27pipeline    # Job name
 #SBATCH --output=slurm_%x.%j.out   # File containing STDOUT output
 #SBATCH --error=slurm_%x.%j.err    # File containing STDERR output. If ommited, use STDOUT.
 
 echo "Starting eval job for PoseMamba on MPI-INF-3DHP (S, 27 frames)"
 
 
-python train_3dhp.py --config configs/pose3d/testing/PoseMamba_train_3dhp_S_27.yaml --evaluate checkpoint/pose3d/PoseMamba_train_3dhp_S_27GT/best_epoch.bin --checkpoint eval/checkpoint_S_27GT
+python train_3dhp.py --config configs/pose3d/testing/PoseMamba_train_3dhp_S_27.yaml --evaluate checkpoint/pose3d/PoseMamba_train_3dhp_S_27GT/best_epoch.bin --checkpoint eval/checkpoint_S_27GTpipeline

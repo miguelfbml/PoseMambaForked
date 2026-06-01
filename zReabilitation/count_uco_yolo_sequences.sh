633935f8e26be341a7a6a7ea57ce7d30754b8028
Miguel Lima <up202108659@up.pt>
2026-06-01 03:46:55 +0100

    a

--- zReabilitation/count_uco_yolo_sequences.sh
+++ zReabilitation/count_uco_yolo_sequences.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --partition=gpu_min8gb     # Reserved partition
-#SBATCH --qos=gpu_min8gb           # QoS level. Must match the partition name. External users must add the suffix "_ext".
+#SBATCH --partition=gpu_min32gb     # Reserved partition
+#SBATCH --qos=gpu_min32gb           # QoS level. Must match the partition name. External users must add the suffix "_ext".
 #SBATCH --job-name=countUcoYolo     # Job name
 #SBATCH --output=slurm_%x.%j.out    # File containing STDOUT output
 #SBATCH --error=slurm_%x.%j.err     # File containing STDERR output. If ommited, use STDOUT.
@@ -19,7 +19,7 @@ python3 count_uco_yolo_sequences.py \
     --model-path "weights/YOLO/best.pt" \
     --output-dir "uco_yolo_sequence_output" \
     --img-size "640" \
-    --batch-size "4" \
+    --batch-size "32" \
     --confidence "0.35" \
     --device "cuda:0" \
     --no-save-videos \
